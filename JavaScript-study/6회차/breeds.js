31de828d95578a07c56c3d8a02bc483e4ea22755
wdohoon <dnjsehgns98@gmail.com>
2024-09-12 14:57:27 +0900

    2024.09.12

--- JavaScript-study/6회차/breeds.js
+++ JavaScript-study/6회차/breeds.js
@@ -1,71 +1,109 @@
-const apiRandomDogs = "https://dog.ceo/api/breeds/image/random/42";
-const apiAllBreads = "https://dog.ceo/api/breeds/list/all";
-const request1 = new XMLHttpRequest();
-const request2 = new XMLHttpRequest();
+const apiRandomDogs = "https://dog.ceo/api/breeds/image/random/42"
+const apiAllBreeds = "https://dog.ceo/api/breeds/list/all"
+const request1 = new XMLHttpRequest()
+const request2 = new XMLHttpRequest()
 
-const header = document.getElementById("header");
-const main = document.getElementById("main");
-const input = document.getElementById("filter-text");
-const button = document.getElementById("filter-button");
-const select = document.getElementById("filter-select");
+const header = document.getElementById("header")
+const main = document.getElementById("main")
+const input = document.getElementById("filter-text")
+const button = document.getElementById("filter-button")
+const select = document.getElementById("filter-select")
+const more = document.getElementById("more")
+const top_btn = document.getElementById("top_btn")
+const reset_btn = document.getElementById("reset_btn")
 
 const currentDogs = []
 
 function displayDogs(item){
-    const dogImgDiv = document.createElement("div");
-    dogImgDiv.classList.add("flex-item");
+    const dogImgDiv = document.createElement("div")
+    dogImgDiv.classList.add("flex-item")
     dogImgDiv.innerHTML = `
-                <img src=${item}>
-            `
-    main.appendChild(dogImgDiv);
+    <img src=${item}>
+  `
+    main.appendChild(dogImgDiv)
 }
 
-addEventListener("load", function(){
+
+window.addEventListener("load", function(){
+
     // 강아지 사진 뿌리기
-    request1.open("get", apiRandomDogs)
+    request1.open("GET", apiRandomDogs)
     request1.addEventListener("load", function(){
-        const response = JSON.parse(request1.response);
+        const response = JSON.parse(request1.response)
         response.message.forEach(function(item){
-            currentDogs.push(item);
-            displayDogs(item);
-        })
+            currentDogs.push(item)
+            displayDogs(item)
+        });
     })
     request1.send()
 
-    // select 에 견종 정보 뿌리기
-    request2.open("get", apiAllBreads)
+    // 견종 텍스트 뿌리기
+    request2.open("GET", apiAllBreeds)
     request2.addEventListener("load", function(){
-        const response = JSON.parse(request2.response);
+        const response = JSON.parse(request2.response)
         Object.keys(response.message).forEach(function(item){
-            const option = document.createElement("option");
-            option.textContent = item;
-            option.value = item;
-            select.appendChild(option);
+            const option = document.createElement("option")
+            option.textContent = item
+            option.value = item
+            select.appendChild(option)
         });
-    });
-    request2.send();
-});
+    })
+    request2.send()
+})
 
-button.addEventListener("click", function (e) {
+button.addEventListener("click", function(){
     main.innerHTML = ""
     let filteredDogs = currentDogs.filter(function(item){
-        return item.indexOf(input.value) !== -1;
+        return item.indexOf(input.value) !== -1
     })
-
     input.value = ""
-
     filteredDogs.forEach(function(item){
-        displayDogs(item);
-    })
+        displayDogs(item)
+    });
 })
 
 select.addEventListener("change", function(){
     main.innerHTML = ""
     let filteredDogs = currentDogs.filter(function(item){
-        return item.indexOf(select.value) !== -1;
+        return item.indexOf(select.value) !== -1
     })
 
     filteredDogs.forEach(function(item){
-        displayDogs(item);
-    })
+        displayDogs(item)
+    });
 })
+
+more.addEventListener("click", function(){
+    // 강아지 사진 더 불러와서 추가하고 뿌리기
+    request1.open("GET", apiRandomDogs)
+    request1.addEventListener("load", function(){
+        const response = JSON.parse(request1.response)
+        response.message.forEach(function(item){
+            currentDogs.push(item)
+            displayDogs(item)
+        });
+    })
+    request1.send()
+})
+
+top_btn.addEventListener("click", function(){
+    window.scrollTo({ top: 0 })
+})
+
+reset_btn.addEventListener("click", function(){
+    // 기존 강아지 이미지를 모두 지움
+    main.innerHTML = "";
+    currentDogs.length = 0; // currentDogs 배열도 초기화
+
+    // 새로운 강아지 이미지를 받아옴
+    request1.open("GET", apiRandomDogs);
+    request1.addEventListener("load", function(){
+        const response = JSON.parse(request1.response);
+        response.message.forEach(function(item){
+            currentDogs.push(item);
+            displayDogs(item);
+        });
+    });
+    request1.send();
+});
+
