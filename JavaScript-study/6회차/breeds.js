4309638e63f2e3c70c8584d3caa6dd59345ea160
wdohoon <dnjsehgns98@gmail.com>
2024-09-12 14:25:13 +0900

    2024.09.12

--- JavaScript-study/6회차/breeds.js
+++ JavaScript-study/6회차/breeds.js
@@ -11,12 +11,61 @@ const select = document.getElementById("filter-select");
 
 const currentDogs = []
 
+function displayDogs(item){
+    const dogImgDiv = document.createElement("div");
+    dogImgDiv.classList.add("flex-item");
+    dogImgDiv.innerHTML = `
+                <img src=${item}>
+            `
+    main.appendChild(dogImgDiv);
+}
+
 addEventListener("load", function(){
     // 강아지 사진 뿌리기
     request1.open("get", apiRandomDogs)
     request1.addEventListener("load", function(){
         const response = JSON.parse(request1.response);
-
+        response.message.forEach(function(item){
+            currentDogs.push(item);
+            displayDogs(item);
+        })
     })
     request1.send()
+
+    // select 에 견종 정보 뿌리기
+    request2.open("get", apiAllBreads)
+    request2.addEventListener("load", function(){
+        const response = JSON.parse(request2.response);
+        Object.keys(response.message).forEach(function(item){
+            const option = document.createElement("option");
+            option.textContent = item;
+            option.value = item;
+            select.appendChild(option);
+        });
+    });
+    request2.send();
+});
+
+button.addEventListener("click", function (e) {
+    main.innerHTML = ""
+    let filteredDogs = currentDogs.filter(function(item){
+        return item.indexOf(input.value) !== -1;
+    })
+
+    input.value = ""
+
+    filteredDogs.forEach(function(item){
+        displayDogs(item);
+    })
+})
+
+select.addEventListener("change", function(){
+    main.innerHTML = ""
+    let filteredDogs = currentDogs.filter(function(item){
+        return item.indexOf(select.value) !== -1;
+    })
+
+    filteredDogs.forEach(function(item){
+        displayDogs(item);
+    })
 })
