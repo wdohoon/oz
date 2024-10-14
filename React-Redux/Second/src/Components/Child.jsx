bc3b1c06c3f8a8a63e4b52bf8d39cc845d270fe3
원도훈 <dnjsehgns98@gmail.com>
2024-10-14 18:52:11 +0900

    2024.10.14

--- React-Redux/Second/src/Components/Child.jsx
+++ React-Redux/Second/src/Components/Child.jsx
@@ -1,8 +1,12 @@
 
-function Child() {
+function Child(info) {
     return (
         <div style={{border: 'solid 1px red', padding: '10px'}}>
-            자식 컴포넌트
+            <p>이름</p>
+            <div>{info.name}</div>
+            <p>나이</p>
+            <div>{info.age}</div>
+            { info.isMarried ? (<div>기혼</div>) : (<div>미혼</div>) }
         </div>
     )
 }
--- React-Redux/Second/src/Components/Parent.jsx
+++ React-Redux/Second/src/Components/Parent.jsx
@@ -1,10 +1,60 @@
 import Child from "./Child.jsx";
+import {useState} from "react";
 
 const Parent = () => {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [isMarried, setIsMarried] = useState(false);
+
+    const [form, setForm] = useState(
+        [
+            {
+                name: '',
+                age: 0,
+                isMarried: false
+            }
+        ]
+    )
+    const handleAdd = () => {
+        setForm(
+            [
+                ...form,
+                {
+                    name: name,
+                    age: age,
+                    isMarried: isMarried
+                }
+            ]
+        )
+    }
+
     return (
-        <div style={{ border: "solid 1px blue" }}>
-            부모 컴포넌트
-            <Child />
+        <div style={{border: "solid 1px blue", padding: '10px'}}>
+            <p>이름</p>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <p>나이</p>
+            <input
+                type="number"
+                value={age}
+                onChange={(e) => (setAge(e.target.value))}
+            />
+            <p>결혼 유무</p>
+            <input
+                type="checkbox"
+                checked={isMarried}
+                onChange={(e) => setIsMarried(e.target.checked)}
+            />
+            <button onClick={handleAdd}>추가</button>
+            {
+                form.map((item) => (
+                    <Child name={item.name} age={item.age} isMarried={item.isMarried}/>
+
+                ))
+            }
         </div>
     )
 }
