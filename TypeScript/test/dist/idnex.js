a24eb1e2b36f39863c1c014cc06a1ccea27b98d2
원도훈 <dnjsehgns98@gmail.com>
2024-12-04 14:01:08 +0900

    2024.12.04

---
+++ TypeScript/test/dist/idnex.js
@@ -0,0 +1,12 @@
+"use strict";
+// console.log("hello typescript")
+//
+// const nums: number[] = []
+//
+// const a = 10 + nums[0]
+Object.defineProperty(exports, "__esModule", { value: true });
+// const someString = "typescript!"
+//
+// someString.endWidth("!")
+const util_1 = require("./util");
+(0, util_1.printName)("typescript");
---
+++ TypeScript/test/idnex.ts
@@ -0,0 +1,13 @@
+// console.log("hello typescript")
+//
+// const nums: number[] = []
+//
+// const a = 10 + nums[0]
+
+// const someString = "typescript!"
+//
+// someString.endWidth("!")
+
+import {printName} from "./util";
+
+printName("typescript");
