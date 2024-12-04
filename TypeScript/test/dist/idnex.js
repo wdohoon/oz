096b94babf836ea28d4a1835001ab6f71a0d63ba
원도훈 <dnjsehgns98@gmail.com>
2024-12-04 14:05:59 +0900

    2024.12.04

--- TypeScript/test/dist/idnex.js
+++ TypeScript/test/dist/idnex.js
@@ -1,12 +1,40 @@
-"use strict";
-// console.log("hello typescript")
-//
-// const nums: number[] = []
-//
-// const a = 10 + nums[0]
-Object.defineProperty(exports, "__esModule", { value: true });
-// const someString = "typescript!"
-//
-// someString.endWidth("!")
-const util_1 = require("./util");
-(0, util_1.printName)("typescript");
+const a = '';
+const b = '';
+const c = ``;
+let myName = 'MinJae';
+let message = `Hello, ${myName}`;
+myName.toLocaleUpperCase();
+let n = 100;
+let count = 10;
+let price = 9.99;
+let temperature = -15;
+let distance = 3.4e-5;
+let total = count * price;
+let average = total / 2;
+let infinity = Infinity;
+let minusInfinity = -Infinity;
+let iAmNotNumber = NaN;
+
+let isOpen = true;
+let isCompleted = false;
+if (isOpen) {
+    console.log('Hello we are open');
+}
+if (!isCompleted) {
+    console.log('job not complete');
+}
+let isAvailable = isOpen && !isCompleted;
+let user = null;
+function login(userName) {
+    user = userName;
+}
+function logout() {
+    user = null;
+}
+login('Hi');
+logout();
+
+let someValue;
+someValue.toString();
+someValue = false;
+someValue.toFixed();
--- TypeScript/test/idnex.ts
+++ TypeScript/test/idnex.ts
@@ -1,13 +1,52 @@
-// console.log("hello typescript")
-//
-// const nums: number[] = []
-//
-// const a = 10 + nums[0]
+const a: string = '';
+const b: string = '';
+const c: string = ``;
 
-// const someString = "typescript!"
-//
-// someString.endWidth("!")
+let myName: string = 'MinJae';
+let message: string = `Hello, ${myName}`;
+myName.toLocaleUpperCase();
 
-import {printName} from "./util";
+// 숫자 number
+let n: number = 100;
+let count: number = 10;
+let price: number = 9.99;
+let temperature: number = -15;
+let distance: number = 3.4e-5;
 
-printName("typescript");
+let total: number = count * price;
+let average: number = total / 2;
+
+let infinity: number = Infinity;
+let minusInfinity: number = -Infinity;
+let iAmNotNumber: number = NaN;
+
+// 불리언 boolean
+let isOpen: boolean = true;
+let isCompleted: boolean = false;
+
+if (isOpen) {
+    console.log('Hello we are open');
+}
+if (!isCompleted) {
+    console.log('job not complete');
+}
+
+let isAvailable: boolean = isOpen && !isCompleted;
+
+// null
+let user: string | null = null;
+
+function login(userName: string) {
+    user = userName;
+}
+function logout() {
+    user = null;
+}
+login('MINJAE');
+logout();
+
+// any
+let someValue: any;
+someValue.toString();
+someValue = false;
+someValue.toFixed();
